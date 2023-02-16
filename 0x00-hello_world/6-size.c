d61746213c22643f79665a6ea038c32f1c69ed0c
Mario-Mellek <mario.samy266@gmail.com>
2023-02-16 20:48:16 +0000

    Added a new line to the last statment

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %ld byte(s)", sizeof(char));
 printf("\nSize of an int: %ld byte(s)", sizeof(int));
 printf("\nSize of a long int: %ld byte(s)", sizeof(long int));
 printf("\nSize of a long long int: %ld byte(s)", sizeof(long long int));
-printf("\nSize of a float: %ld byte(s)", sizeof(float));
+printf("\nSize of a float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
