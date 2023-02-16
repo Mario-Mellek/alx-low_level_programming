64fb826c1feb814f4a3e1a752abc9c28f38142f2
Mario-Mellek <mario.samy266@gmail.com>
2023-02-16 20:23:15 +0000

    A C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point, no arguments
+ *
+ * prints the senetence
+ * "with proper grammar, but the outcome is a piece of art,"
+ * Ending with a new line
+ *
+ * Return: Returns 0 when terminates
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
