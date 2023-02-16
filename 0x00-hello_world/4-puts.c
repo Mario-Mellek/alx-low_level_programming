20d49e64ea3f7452685ea84f91288a2236c535e7
Mario-Mellek <mario.samy266@gmail.com>
2023-02-16 18:45:58 +0000

    Removed the new line and added comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
-/* Prints "Programming is like building a multilingual puzzle */
+/*
+ * prints "\"Programming is like building a multilingual puzzle"
+ *
+ * The Function puts adds a new line by default
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
