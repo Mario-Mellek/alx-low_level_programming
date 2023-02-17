962f07f8498fc04f534cdaafe157ac1de1cfa04c
Mario-Mellek <mario.samy266@gmail.com>
2023-02-17 16:16:47 +0000

    Fixed Vi indentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@
 
 int main(void)
 {
-printf("Size of a char: %ld byte(s)", sizeof(char));
-printf("\nSize of an int: %ld byte(s)", sizeof(int));
-printf("\nSize of a long int: %ld byte(s)", sizeof(long int));
-printf("\nSize of a long long int: %ld byte(s)", sizeof(long long int));
-printf("\nSize of a float: %ld byte(s)\n", sizeof(float));
-return (0);
+	printf("Size of a char: %ld byte(s)", sizeof(char));
+	printf("\nSize of an int: %ld byte(s)", sizeof(int));
+	printf("\nSize of a long int: %ld byte(s)", sizeof(long int));
+	printf("\nSize of a long long int: %ld byte(s)", sizeof(long long int));
+	printf("\nSize of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
 }
