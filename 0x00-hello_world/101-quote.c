f7e763201f6660af945cfd2bb6cc2027f793bc39
Mario-Mellek <mario.samy266@gmail.com>
2023-02-17 01:43:11 +0000

    Editing code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
-#include <sys/syscall.h>
+/* #include <sys/syscall.h> */
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -10,13 +11,15 @@
  * syscall assembly instruction.i
  * reference: https://jameshfisher.com/2018/02/19/how-to-syscall-in-c/
  *
+ * const char msg[] =
+ * "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+ * syscall(SYS_write, 1, msg, sizeof(msg));
+ *
  * Return: returns 1
  */
 
 int main(void)
 {
-const char msg[] =
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 2, msg, sizeof(msg));
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
