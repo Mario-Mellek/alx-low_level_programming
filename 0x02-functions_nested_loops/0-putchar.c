87cf326c264be20dd22a7516999b2915933fe06d
Mario-Mellek <mario.samy266@gmail.com>
2023-02-21 21:55:17 +0000

    Edited styles and output

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
 #include "main.h"
 
+/**
+ *
+ * main - A program that prints _putchar, 
+ * followed by a new line.
+ *
+ * Return: returns 0
+ */
+
 int main(void)
 {
 	char input[] = "_putchar";
 	int size = 0;
 
-	while (size <= 8)
+	while (size < 8)
 	{
 		_putchar(input[size]);
 		size++;
