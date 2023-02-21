74fba0a873d13835291d2efff88a43da15fb5fe9
Mario-Mellek <mario.samy266@gmail.com>
2023-02-21 20:57:09 +0000

    Reverting to previous commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,13 @@
-#include <main.h>
+#include <stdio.h>
+/**
+ * main - Prints prints _putchar,
+ * followed by a new line.
+ *
+ * Return: returns 0
+ */
 
 int main(void)
 {
-	_putchar("_");
+	printf("_putchar\n");
 	return (0);
 }
