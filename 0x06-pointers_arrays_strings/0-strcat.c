d64368ffe549eda4a12e5c37df6fc6d064ca16a9
Mario-Mellek <mario.samy266@gmail.com>
2023-03-02 20:26:04 +0000

    added a conditional statment

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,9 +4,15 @@ char *_strcat(char *dest, char *src)
 {
 	int i, j;
 	for(i = 0; dest[i] != '\0'; i++);
-	for (j = 0 ; j < i && src[j] != '\0'; j++)
+	for (j = 0 ; j < i ; j++)
 	{ 
-		dest[j + i] = src[j];
+		if(src[j] != '\0')
+		{
+			dest[j + i] = src[j];
+		}else
+		{
+			continue;
+		}
 	}
 	dest[j + i] = '\0';
 	return (dest);
