df39a7ad44d60064e0463033ddee75004101bfe6
Mario-Mellek <mario.samy266@gmail.com>
2023-03-02 23:46:41 +0000

    testing edits

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	for (i = 0; i < n; i++)
 		if(src[i] != '\0')
+		{
 			dest[i] = src[i];
+		} else
+		{
+			continue;
+		}
 	while(i < n)
 	{
 		dest[i] = '\0';
