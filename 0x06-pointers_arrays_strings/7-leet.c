7da863019a072e3d72d4bd82c18484b0f9fe101d
Mario-Mellek <mario.samy266@gmail.com>
2023-03-03 03:07:12 +0000

    testing

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,34 +5,22 @@
  * @s: string param
  * Return: returns array
  */
-
 char *leet(char *s)
 {
-	int i = 0;
+	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == 'a' || s[i] == 'A')
-		{
 			s[i] = '4';
-		}
-		else if (s[i] == 'e' || s[i] == 'E')
-		{
+		if (s[i] == 'e' || s[i] == 'E')
 			s[i] = '3';
-		}
-		else if (s[i] == 'o' || s[i] == 'O')
-		{
+		if (s[i] == 'o' || s[i] == 'O')
 			s[i] = '0';
-		}
-		else if (s[i] == 't' || s[i] == 'T')
-		{
+		if (s[i] == 't' || s[i] == 'T')
 			s[i] = '7';
-		}
-		else if (s[i] == 'l' || s[i] == 'L')
-		{
+		if (s[i] == 'l' || s[i] == 'L')
 			s[i] = '1';
-		}
-		i++;
 	}
 	return (s);
 }
