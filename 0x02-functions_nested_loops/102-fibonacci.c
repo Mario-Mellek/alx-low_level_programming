432d9f0200e0c9554559c3dcfc471a3a5c527f22
Mario-Mellek <mario.samy266@gmail.com>
2023-03-18 22:49:27 +0200

    Code edited
    Edited the code so that it includes the first 50 fibonacci numbers
    instead of up to 50

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	int limit = 50, i;
-	int j = 1, temp;
+	long int limit = 20365011074;
+	long int j = 1, i, temp;
 
-	for (i = 0; i < limit;)
+	for (i = 0; i <= limit;)
 	{
-		if (i == 34)
+		if (i == limit)
 		{
-			printf("%d", i);
+			printf("%ld", i);
 			break;
 		}
-		printf("%d, ", i);
+		printf("%ld, ", i);
 		temp = i + j;
 		i = j;
 		j = temp;
