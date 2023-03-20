1e469c3fca9cce7888db931fe8cb911e8eaf3a85
Mario-Mellek <mario.samy266@gmail.com>
2023-03-20 22:51:32 +0200

    Added comments

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,12 @@
 #ifndef MACRO_H
 #define MACRO_H
 
+/**
+ * The ABS(x) function-like macro is a preprocessor
+ * macro in C that computes the absolute value of a number x.
+ * The absolute value of a number is its distance from zero.
+ */
+
 #define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif
