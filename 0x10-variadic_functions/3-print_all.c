a740c4736c059f79709ca87e32f67199207bb4b3
Mario-Mellek <mario.samy266@gmail.com>
2023-03-24 04:55:05 +0200

    editing null output

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -34,7 +34,7 @@ void print_all(const char * const format, ...)
 			case's':
 				str = va_arg(args, char *);
 				if (!str)
-					str = "nill";
+					str = "(nil)";
 				printf("%s", str);
 				break;
 			default:
