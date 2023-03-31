55bdb6e437f21f4d9d1407a88c5c954a769e5f09
Mario-Mellek <mario.samy266@gmail.com>
2023-03-31 03:35:01 +0200

    Used the GCC constructor attribute to execute a function before main() is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * startUp - A function that uses the GCC
+ * constructor attribute to execute a function
+ * before main() is executed.
+ */
+
+void startUp(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
