f44547810145fd16014b8b709eaabb3e6d8ea654
Mario-Mellek <mario.samy266@gmail.com>
2023-04-05 02:00:17 +0200

    no leaks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	if (*head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	*head = NULL;
+}
