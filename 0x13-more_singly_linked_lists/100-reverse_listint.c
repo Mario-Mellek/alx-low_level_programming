5482f7b690a41ff955acfe3932807da6b60409d3
Mario-Mellek <mario.samy266@gmail.com>
2023-04-06 02:27:34 +0200

    Correct output

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	if (*head == NULL)
+		return NULL;
+
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
+}
