8cdb6f19f6b407acf693132370acdddabf0c59bb
Mario-Mellek <mario.samy266@gmail.com>
2023-04-06 04:06:30 +0200

    A function that frees a listint_t list.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * free_listint_safe - A function that frees a listint_t list.
+ * @h: pointer to head pointer
+ * Return: the size of the list that was free’d
+ */
+
+
+
 size_t free_listint_safe(listint_t **h)
 {
 	size_t count = 0;
@@ -7,7 +15,6 @@ size_t free_listint_safe(listint_t **h)
 
 	if (h == NULL)
 		return (0);
-
 
 	while (*h != NULL)
 	{
